7471f7931248d2f13f1b80d0f7f795f287e06417
Nick Lorusso <ndlorusso@gmail.com>
2024-04-29 13:10:30 -0500

    post & put

--- index.js
+++ index.js
@@ -17,7 +17,23 @@ app.get('/api/flavors', async (req, res, next) => {
     }
 });
 
-//CREATE
+//READ SINGLE FLAVOR
+app.get('/api/flavors/:id', async (req, res, next) =>  {
+    try {
+        const SQL = /* sql */
+        `
+        SELECT * FROM flavors
+        WHERE id=$1 
+        `;
+        const response = await client.query(SQL, [req.params.id]);
+        res.send(response.rows[0]);
+    } catch (error) {
+        next(error);
+    }
+});
+
+//CREATE 
+// curl localhost:3000/api/flavors -X POST -d '{"txt": "xyz", "is_favorite": true}' -H "Content-Type:application/json"
 app.post('/api/flavors', async(req, res, next) => {
     try {
         const SQL = /* sql */
@@ -33,7 +49,41 @@ app.post('/api/flavors', async(req, res, next) => {
     }
 });
 
+//UPDATE
+// curl localhost:3000/api/flavors/2 -X PUT -d '{"txt": "abc", "is_favorite": false}' -H "Content-Type:application/json"
+app.put('api/flavors/:id', async(req, res, next) =>  {
+    try {
+        const SQL = /* sql */
+        `
+        UPDATE flavors
+        SET txt = $1, is_favorite = $2, updated_at=now()
+        WHERE id=$3
+        RETURNING *
+        `;
+        const response = await client.query(SQL, [req.body.txt, req.body.is_favorite, req.params.id]);
+        res.send(response.rows[0]);
+    } catch (error) {
+        next(error);
+    }
+});
 
+// DELETE
+// curl localhost:3000/api/flavors/1 -X DELETE
+app.delete('api/flavors/:id', async(req, res, next) => {
+    try {
+        const SQL = /* sql */ 
+        `
+        DELETE from flavors
+        WHERE id=$1
+        `;
+        await client.query(SQL, [req.params.id])
+        res.sendStatus(204);
+    } catch (error) {
+        next(error);
+    }
+});
+
+//create table
 const init = async () => {
     await client.connect();
     console.log('connected to database');
@@ -60,9 +110,6 @@ const init = async () => {
     `;
     await client.query(SQL);
     console.log('data seeded');
-
-
-
     const port = process.env.PORT || 3000
     app.listen(port, () => console.log(`listening on port ${port}`));
 };
